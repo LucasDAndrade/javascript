50841282d966c1fc59e5e5c4558be61033700da7
LucasDAndrade <deandradelucas2002@gmail.com>
2023-12-20 18:35:51 -0300

    nada

---
+++ aulas/Z ambiente recreativo Z/node_a0be091538cfa.tmp.js
@@ -0,0 +1,15 @@
+var n = 10;
+
+while (n > 2)
+  console.log (`seu número (${n}) ainda é maior que 2`)
+
+/* 
+
+ (n = 10, n > 2, n--)
+
+ console.log (`seu número (${n}) ainda é maior que 2`)
+
+
+
+  
+  */
--- aulas/Z ambiente recreativo Z/shoosh.js
+++ aulas/Z ambiente recreativo Z/shoosh.js
@@ -1,12 +1,15 @@
 var n = 10;
 
-for (n = 10; n > 2; c++) {
-  console.log (`seu número (${n}) ainda é maior que 2`)
-}
+for (n = 10; n > 2; n--)
+
+ console.log (`seu número (${n}) ainda é maior que 2`)
 
 /* 
 
-while (n > 2) {
+ 
+
+while (n > 2)
   console.log (`seu número (${n}) ainda é maior que 2`)
+
   
   */

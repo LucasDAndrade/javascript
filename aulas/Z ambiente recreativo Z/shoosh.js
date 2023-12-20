dab394dcc9efa1c75bad0ee28b131805bb7ab371
LucasDAndrade <deandradelucas2002@gmail.com>
2023-12-20 16:41:51 -0300

    alguns exercícios adicionados

---
+++ aulas/Z ambiente recreativo Z/shoosh.js
@@ -0,0 +1,12 @@
+var n = 10;
+
+for (n = 10; n > 2; c++) {
+  console.log (`seu número (${n}) ainda é maior que 2`)
+}
+
+/* 
+
+while (n > 2) {
+  console.log (`seu número (${n}) ainda é maior que 2`)
+  
+  */

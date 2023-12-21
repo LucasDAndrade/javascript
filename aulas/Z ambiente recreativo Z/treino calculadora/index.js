5818f09d809dab9563519847fd9f290fc0fa9c87
LucasDAndrade <deandradelucas2002@gmail.com>
2023-12-21 19:44:37 -0300

    Update index.js

--- aulas/Z ambiente recreativo Z/treino calculadora/index.js
+++ aulas/Z ambiente recreativo Z/treino calculadora/index.js
@@ -5,3 +5,14 @@ const soma = document.getElementById('soma')
 const subt = document.getElementById('subt')
 const multi = document.getElementById('multi')
 const divi = document.getElementById('divi')
+
+
+function somar() {
+  if (num1.value.length == 0) {
+    console.log('Verifique os números e tente novamente.')
+  } else if (num2.value.length == 0) {
+    console.log('Verifique os números e tente novamente.')
+  } else {
+    let res
+  }
+}
